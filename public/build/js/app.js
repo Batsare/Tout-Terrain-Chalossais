65e6e2a281920b4ebc92419d223fac90d2b09b54
Batsare <axelbats25@gmail.com>
2018-02-28 13:30:21 +0100

    Css post

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -18,3 +18,25 @@ $( "#post_message_guestbook" ).click(function() {
     }
 
 });
+
+$(document).ready(function () {
+    $('img').on('click', function () {
+        var image = $(this).attr('src');
+        $('#myModal').css("display", "block");
+        $(".img-responsive").attr("src", image);
+        $("#lien_image_article").attr("href", image);
+
+        console.log(image);
+        $('#myModal').on('show.bs.modal', function () {
+            $(".img-responsive").attr("src", image);
+        });
+    });
+
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        $('#myModal').css("display", "none");
+    }
+});
+
