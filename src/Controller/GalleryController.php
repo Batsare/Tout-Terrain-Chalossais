317569e40418bbd4faede989811c7315b17c203a
Batsare <axelbats25@gmail.com>
2018-03-14 08:34:14 +0100

    Gallery controller + routes + templates

---
+++ src/Controller/GalleryController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+Class GalleryController extends Controller
+{
+    public function indexAction()
+    {
+
+    }
+
+    public function addAction()
+    {
+
+    }
+
+    public function deleteAction()
+    {
+
+    }
+
+    public function viewAction()
+    {
+
+    }
+}
