7830c88385507fc011e7d96c71a9a9cf020aef23
Unknown <axelbats25@gmail.com>
2018-03-27 13:33:56 +0200

    maj

---
+++ src/Type/GalleryType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Type;
+
+use App\Repository\CategoryRepository;
+use KMS\FroalaEditorBundle\Form\Type\FroalaEditorType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GalleryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // Arbitrairement, on récupère toutes les catégories qui commencent par "D"
+
+
+        $builder
+            ->add('createdAt',      DateTimeType::class)
+            ->add('title',     TextType::class)
+            ->add('author',    TextType::class)
+            ->add('image',     ImageType::class)
+
+            ->add('published', CheckboxType::class, array(
+                'label' => 'Publier',
+                'required' => false))
+            ->add('save',      SubmitType::class, array(
+                'label' => 'Envoyer'
+            ))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'App\Entity\Gallery'
+        ));
+    }
+}
