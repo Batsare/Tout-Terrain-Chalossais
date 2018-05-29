f192cc9dcefdb7203467b2ca7bc750b643e05315
Unknown <axelbats25@gmail.com>
2018-05-29 21:39:57 +0200

    Commit copie production

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,18 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
+    KMS\FroalaEditorBundle\KMSFroalaEditorBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
 ];
---
+++ public_html/build/js/app.js
@@ -0,0 +1,83 @@
+postGuestbook = $("#posts_guestbook");
+formGuestbook = $("#form_guestbook");
+
+$( "#messages_guestbook" ).click(function() {
+    if( postGuestbook.hasClass('enabled')){
+    }else{
+        formGuestbook.toggleClass('disabled enabled');
+        postGuestbook.toggleClass('enabled disabled');
+    }
+
+});
+
+$( "#post_message_guestbook" ).click(function() {
+    if( formGuestbook.hasClass('enabled')){
+    }else{
+        postGuestbook.toggleClass('disabled enabled');
+        formGuestbook.toggleClass('enabled disabled');
+    }
+
+});
+
+$(document).ready(function () {
+    $(".summernote").summernote({
+        toolbar: [
+            // [groupName, [list of button]]
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']]
+        ]
+    });
+
+    $('img').on('click', function () {
+        $("#lightgallery").lightGallery({
+            thumbnail:true
+        });
+        $("#lightgalleryArticle").lightGallery({
+            selector:'this'
+        });
+
+        // var image = $(this).attr('src');
+        // $('#myModal').css("display", "block");
+        // $(".img-responsive").attr("src", image);
+        // $("#lien_image_article").attr("href", image);
+        //
+        // console.log(image);
+        // $('#myModal').on('show.bs.modal', function () {
+        //     $(".img-responsive").attr("src", image);
+        // });
+    });
+
+//     if(document.getElementsByClassName("close")[0]){
+//         var span = document.getElementsByClassName("close")[0];
+//
+// // When the user clicks on <span> (x), close the modal
+//         span.onclick = function() {
+//             $('#myModal').css("display", "none");
+//         };
+//         $('#myModal').click(function(){
+//             $('#myModal').css("display", "none");
+//         });
+//     }
+
+
+
+    /*
+    $(window).scroll(function () {
+
+        console.log($(window).scrollTop());
+
+        if ($(window).scrollTop() > 70) {
+            $('#nav_bar').addClass('fixed-top');
+        }
+
+        if ($(window).scrollTop() < 71) {
+            $('#nav_bar').removeClass('fixed-top');
+        }
+    });
+     */
+});
+
